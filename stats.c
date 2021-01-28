944e561cb733eb9e4672795610e6a631dce432c0
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-01-28 20:49:00 +0530

    Test Case 1 - Sample

--- stats.c
+++ stats.c
@@ -1,10 +1,39 @@
 #include "stats.h"
 
+struct stats {
+    float average;
+    float Min;
+    float Max;
+}
+
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
     s.average = 0;
-    s.min = 0;
-    s.max = 0;
+    s.min = *numberset[0];
+    s.max = *numberset[0];
+    
+    for(int x=0;x<setlength;x++)
+    {
+       if(s.min>*numberset[x])
+       {
+           s.min=*numberset[x]
+       }
+       else
+       {
+       }   
+        
+       if(s.max<*numberset[x])
+       {
+           s.max=*numberset[x]
+       }
+       else
+       {
+       }  
+      s.average=+ numberset[x];
+    }
+    
+    return s
+    
 }
 
 int emailAlertCallCount = 0;
