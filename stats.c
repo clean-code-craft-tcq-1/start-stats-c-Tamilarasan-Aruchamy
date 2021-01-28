e54d3f1ec8d4db79d055c933d0db8124b42da47b
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-01-28 21:32:37 +0530

    Sample 2

--- stats.c
+++ stats.c
@@ -1,38 +1,33 @@
 #include "stats.h"
 
-struct stats {
-    float average;
-    float Min;
-    float Max;
-}
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
     s.average = 0;
-    s.min = *numberset[0];
-    s.max = *numberset[0];
+    s.min = *numberset;
+    s.max = *numberset;
     
     for(int x=0;x<setlength;x++)
     {
-       if(s.min>*numberset[x])
+       if(s.min>*(numberset+x))
        {
-           s.min=*numberset[x]
+           s.min=*(numberset+x);
        }
        else
        {
        }   
         
-       if(s.max<*numberset[x])
+       if(s.max<*(numberset+x))
        {
-           s.max=*numberset[x]
+           s.max=*(numberset+x);
        }
        else
        {
        }  
-      s.average=+ numberset[x];
+      s.average+=*(numberset+x);
     }
-    
-    return s
+    s.average/=setlength;
+    return s;
     
 }
 
